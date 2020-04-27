2d886f27dd871b579a955c273eaf979d91be7316
ZIN MYO <overmidnight12@gmail.com>
2020-04-27 13:30:01 -0700

    my changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,9 +3,14 @@
 @section('title', 'Sign Up for an Account')
 
 @section('content')
+
+<br><br>
 <div class="container">
-    <div class="auth-pages">
-        <div>
+    <div class="row">
+        <div class="col-lg-6 offset-3">
+            <h2>YOUR ACCOUNT FOR EVERYTHING <br>
+            ACTIVE SHOP</h2>
+
             @if (session()->has('success_message'))
             <div class="alert alert-success">
                 {{ session()->get('success_message') }}
@@ -19,43 +24,39 @@
                 </ul>
             </div>
             @endif
-            <h2>Create Account</h2>
-            <div class="spacer"></div>
 
+            
             <form method="POST" action="{{ route('register') }}">
                 {{ csrf_field() }}
 
-                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Name" required autofocus>
+              <div class="form-group">
+                
+                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}" placeholder="Name">
+              </div>
 
+              <div class="form-group">
                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email" required>
+              </div>
+              <div class="form-group">
+                 <input id="password" type="password" class="form-control" name="password" placeholder="Password" placeholder="Password" required>
+              </div>
 
-                <input id="password" type="password" class="form-control" name="password" placeholder="Password" placeholder="Password" required>
-
+              <div class="form-group">
                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password"
                     required>
-
-                <div class="login-container">
-                    <button type="submit" class="auth-button">Create Account</button>
-                    <div class="already-have-container">
-                        <p><strong>Already have an account?</strong></p>
-                        <a href="{{ route('login') }}">Login</a>
-                    </div>
-                </div>
-
+              </div>
+              
+              <button type="button" class="btn btn-dark btn-lg btn-block">Register</button>
             </form>
+            <br>
+            <p style="text-align: center; color: grey;">
+                Already a member?
+                <a href="{{ route('login') }}" style="color: black;">Sign in.</a>
+            </p>
+             
         </div>
-
-        <div class="auth-right">
-            <h2>New Customer</h2>
-            <div class="spacer"></div>
-            <p><strong>Save time now.</strong></p>
-            <p>Creating an account will allow you to checkout faster in the future, have easy access to order history and customize your experience to suit your preferences.</p>
-
-            &nbsp;
-            <div class="spacer"></div>
-            <p><strong>Loyalty Program</strong></p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt debitis, amet magnam accusamus nisi distinctio eveniet ullam. Facere, cumque architecto.</p>
-        </div>
-    </div> <!-- end auth-pages -->
+    </div>
 </div>
+<br><br><br><br><br>
+
 @endsection
