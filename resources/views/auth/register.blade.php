a98e80b75dc1da643e5f6c6a41bc0449104697e5
ZIN MYO <overmidnight12@gmail.com>
2020-04-29 16:53:08 -0700

    my changes

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,12 +4,45 @@
 
 @section('content')
 
-<br><br>
+<style type="text/css">
+  .card{
+    padding: 20px;
+    border-radius: 0;
+  }
+  a{
+    color: black;
+    text-decoration: underline;
+  }
+  a:hover{
+    color: black;
+    text-decoration: underline;
+  }
+
+  .btn-outline-dark{
+    border-radius: 0;
+    width: 150px;
+  }
+  .btn-outline-dark:hover{
+    background: white;
+    color: black;
+    cursor: pointer;
+  }
+  .btn-dark{
+    border-radius: 0;
+  }
+  .spring-btn-black{
+            clear:both; background: #000; border: none; color: #fff; font-size: 1em; padding: 10px; cursor: pointer; font-size: 17px;
+      }
+
+</style>
+
+<br><br><br>
 <div class="container">
     <div class="row">
-        <div class="col-lg-6 offset-3">
-            <h2>YOUR ACCOUNT FOR EVERYTHING <br>
-            ACTIVE SHOP</h2>
+        <div class="col-lg-4 col-sm-12 col-xs-12 offset-4">
+          <div class="card">
+            <h2 style="text-align: center"> ACTIVE SHOP</h2>
+            <br>
 
             @if (session()->has('success_message'))
             <div class="alert alert-success">
@@ -46,17 +79,26 @@
                     required>
               </div>
               
-              <button type="submit" class="btn btn-dark btn-lg btn-block">Register</button>
+              <button type="submit" class="spring-btn-black ">REGISTER <span class="fa fa-arrow-right" style="margin-left: 9px;"></span></button>
             </form>
-            <br>
+           <br>
+
+
+            <div class="btn btn-outline-dark">FACEBOOK <img src="https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512" width="25" height="25"></div><br>
+
+                <div class="btn btn-outline-dark">GOOGLE <img src="https://img.icons8.com/color/480/google-logo.png" width="25" height="25"></div>
+               <br>
             <p style="text-align: center; color: grey;">
                 Already a member?
                 <a href="{{ route('login') }}" style="color: black;">Sign in.</a>
             </p>
-             
+
+            <p>By clicking 'Submit' you agree to the <a href=""> Creators Club Terms & Conditions</a>, <a href=""> adidas Terms & Conditions </a>and the adidas Privacy Policy</p>
+             </div>
         </div>
     </div>
 </div>
-<br><br><br><br><br>
+
+<div style="height: 230px;"></div>
 
 @endsection
